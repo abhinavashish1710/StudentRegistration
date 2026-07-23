9c7e76973dd8284588368cf2ba1f2f2939beb196
abhinavashish1710 <abhinavashish1106@gmail.com>
2026-07-23 20:49:23 +0530

    Initial commit - Student Registration CRUD

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StudentRegistration.Models;
+
+namespace StudentRegistration.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : IdentityDbContext(options)
+    {
+        public DbSet<Course> Courses { get; set; }
+    }
+}
---
+++ Models/Course.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentRegistration.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string CourseCode { get; set; }
+
+        [Required]
+        public string CourseName { get; set; }
+
+        [Range(1, 10)]
+        public int Credits { get; set; }
+
+        [Required]
+        public string Instructor { get; set; }
+    }
+}
